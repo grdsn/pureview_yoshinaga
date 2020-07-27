6dc23ea127ccbbf82bd5f8c5daff77b7c6c1c189
s3a2 <s3a2@KCSF0437>
2020-07-27 14:24:13 +0900

    ぷれびゅ

--- bottun_pre/Form1.Designer.cs
+++ bottun_pre/Form1.Designer.cs
@@ -34,6 +34,16 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.pureButton1 = new System.Windows.Forms.Button();
+            this.pureButton2 = new System.Windows.Forms.Button();
+            this.pureButton3 = new System.Windows.Forms.Button();
+            this.pureButton4 = new System.Windows.Forms.Button();
+            this.pureButton5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -74,6 +84,11 @@
             this.tableLayoutPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.tableLayoutPanel.ColumnCount = 1;
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Controls.Add(this.pureButton1, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.pureButton2, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.pureButton3, 0, 2);
+            this.tableLayoutPanel.Controls.Add(this.pureButton4, 0, 3);
+            this.tableLayoutPanel.Controls.Add(this.pureButton5, 0, 4);
             this.tableLayoutPanel.Location = new System.Drawing.Point(389, 285);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 5;
@@ -85,18 +100,107 @@
             this.tableLayoutPanel.Size = new System.Drawing.Size(127, 129);
             this.tableLayoutPanel.TabIndex = 3;
             // 
+            // pureButton1
+            // 
+            this.pureButton1.Location = new System.Drawing.Point(3, 3);
+            this.pureButton1.Name = "pureButton1";
+            this.pureButton1.Size = new System.Drawing.Size(121, 19);
+            this.pureButton1.TabIndex = 0;
+            this.pureButton1.UseVisualStyleBackColor = true;
+            this.pureButton1.Visible = false;
+            this.pureButton1.Click += new System.EventHandler(this.pureButton1_Click);
+            // 
+            // pureButton2
+            // 
+            this.pureButton2.Location = new System.Drawing.Point(3, 28);
+            this.pureButton2.Name = "pureButton2";
+            this.pureButton2.Size = new System.Drawing.Size(121, 19);
+            this.pureButton2.TabIndex = 1;
+            this.pureButton2.UseVisualStyleBackColor = true;
+            this.pureButton2.Visible = false;
+            // 
+            // pureButton3
+            // 
+            this.pureButton3.Location = new System.Drawing.Point(3, 53);
+            this.pureButton3.Name = "pureButton3";
+            this.pureButton3.Size = new System.Drawing.Size(121, 19);
+            this.pureButton3.TabIndex = 2;
+            this.pureButton3.UseVisualStyleBackColor = true;
+            this.pureButton3.Visible = false;
+            // 
+            // pureButton4
+            // 
+            this.pureButton4.Location = new System.Drawing.Point(3, 78);
+            this.pureButton4.Name = "pureButton4";
+            this.pureButton4.Size = new System.Drawing.Size(121, 19);
+            this.pureButton4.TabIndex = 3;
+            this.pureButton4.UseVisualStyleBackColor = true;
+            this.pureButton4.Visible = false;
+            // 
+            // pureButton5
+            // 
+            this.pureButton5.Location = new System.Drawing.Point(3, 103);
+            this.pureButton5.Name = "pureButton5";
+            this.pureButton5.Size = new System.Drawing.Size(121, 23);
+            this.pureButton5.TabIndex = 4;
+            this.pureButton5.UseVisualStyleBackColor = true;
+            this.pureButton5.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(522, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "←選択されているファイルを表示します";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(524, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 5;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(535, 390);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 6;
+            this.closeButton.Text = "削除";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(535, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.tableLayoutPanel);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.webBrowser1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -107,6 +211,15 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button pureButton1;
+        private System.Windows.Forms.Button pureButton2;
+        private System.Windows.Forms.Button pureButton3;
+        private System.Windows.Forms.Button pureButton4;
+        private System.Windows.Forms.Button pureButton5;
     }
 }
 
--- bottun_pre/Form1.cs
+++ bottun_pre/Form1.cs
@@ -17,47 +17,105 @@ namespace bottun_pre
         {
             InitializeComponent();
         }
-        private String[] htmlFile;
-        int cnt = 0;
 
-        //挿入位置
-        int insertRow = 0;
-        int insertColumn = 0;
+        string[] htmlFile = new string[5];      // ファイル名を保存する
+        int cnt = 0;                            // ボタンの表示数をカウント
+        Button[] buttons = new Button[5];       // ボタンの情報を保持する
 
         private void button1_Click(object sender, EventArgs e)
         {
 
-            // ファイル名を保存する
-            if(this.htmlFile[cnt] == null && cnt <= 5) { }
+            // ！”プレビューを表示する前にデータの保存が必要です”
+            // 取得するファイルをlistBoxで表現
 
-            htmlFile[cnt] = listBox1.SelectedItem.ToString();
 
-            // ファイルの読み込み
-            StreamReader sr = new StreamReader(@"../../../html/" + htmlFile, Encoding.GetEncoding("UTF-8"));
-            string str = sr.ReadToEnd();
-            sr.Close();
-
-            // title取得
-            int start = str.IndexOf("<title>");
-            int end = str.IndexOf("</title>");
-            // str.Substring(開始位置,取り出す文字数)
-            string title = str.Substring(start + 7, end - (start + 7));
-
-            if (tableLayoutPanel.GetControlFromPosition(insertColumn, insertRow) == null)
-            {
-                Button newButton = new Button();
-                newButton.Text = title;
-                newButton.Name = "titleButton" + cnt;
-                tableLayoutPanel.Controls.Add(newButton, insertColumn++, insertRow++);
+            // 同じものは表示しない
+            Boolean flg = true;
+            foreach (string oldTitle in htmlFile) {
+                if (cnt == 5 || oldTitle == listBox1.SelectedItem.ToString())
+                {
+                    flg = false;
+                    MessageBox.Show("重複で表示はできないよ！または、これ以上表示できないよ！的な");
+                }
             }
 
-            //listBox1.Items.Add(title);
+            if (flg == true)
+            {
+                // ファイル名を保持する
+                htmlFile[cnt] = listBox1.SelectedItem.ToString();
+                label1.Text = listBox1.SelectedItem.ToString();
+
+                // ファイルの読み込み
+                StreamReader sr = new StreamReader("../../../html/" + htmlFile[cnt], Encoding.GetEncoding("UTF-8"));
+                string str = sr.ReadToEnd();
+                sr.Close();
+
+
+                // title取得
+                int start = str.IndexOf("<title>");
+                int end = str.IndexOf("</title>");
+                // str.Substring(開始位置,取り出す文字数)
+                string title = str.Substring(start + 7, end - (start + 7));
+
+                int Numcnt = 0;
+                // ボタンを配置する
+                for(int i = 0; buttons[cnt] != null; i++)
+                {
+                    Numcnt++;
+                }
+
+                if (Numcnt == 0)
+                {
+                    //buttonName.Text = title;
+                    //buttonName.Name = title;
+                    buttons[cnt] = pureButton1;
+                    pureButton1.Text = label1.Text;
+                    pureButton1.Name = label1.Text;
+                    pureButton1.Visible = true;
+                }
+                else if(Numcnt == 1)
+                {
+                    buttons[cnt] = pureButton2;
+                    pureButton2.Text = label1.Text;
+                    pureButton2.Name = label1.Text;
+                    pureButton2.Visible = true;
+                }
+                else if(Numcnt == 2)
+                {
+                    buttons[cnt] = pureButton3;
+                    pureButton3.Text = label1.Text;
+                    pureButton3.Name = label1.Text;
+                    pureButton3.Visible = true;
+                }
+                cnt++;
+            }
+
         }
 
-        private void btnclick(object sender, System.EventArgs e)
+        private void closeButton_Click(object sender, EventArgs e)
         {
-            Button btn = (Button)sender;
-            webBrowser1.Navigate("../../../../tset03_pure/" + btn.Name);
+            for (int l = 0; l <= 5; l++)
+            {
+                buttons[--cnt].Visible = false;                               // 改善要
+                htmlFile[cnt] = null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // testボタンの処理なので関係ないです
+            webBrowser1.Navigate("file:///C:/Users/S3A2/source/repos/bottun_pre/html/hello.html");
+        }
+
+        private void pureButton1_Click(object sender, EventArgs e)
+        {
+            // 動的に表示されたボタンを押すとWeb表示
+            webBrowser1.Navigate("file:///C:/Users/S3A2/source/repos/bottun_pre/html/" + pureButton1.Text);
+        }
+        private void pureButton2_Click(object sender, EventArgs e)
+        {
+            // 動的に表示されたボタンを押すとWeb表示
+            webBrowser1.Navigate("file:///C:/Users/S3A2/source/repos/bottun_pre/html/" + pureButton2.Text);
         }
     }
 }
